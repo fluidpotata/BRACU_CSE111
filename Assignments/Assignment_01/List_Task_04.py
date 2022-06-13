c105edad412bf772e45a351e426e76992ee73ca3
Ibrahim Alif <100870354+itsalif1608@users.noreply.github.com>
2022-06-13 20:45:22 +0600

    Create List_Task_04.py

--- Assignments/Assignment_01/List_Task_04.py
+++ Assignments/Assignment_01/List_Task_04.py
@@ -1,1 +1,11 @@
-#TBD
+inp=str(input('Enter n and k: ')).split(" ")
+student = int(inp[0])
+time = int(inp[1])
+count =0
+lst = str(input('Enter: ')).split(" ")
+for i in range(len(lst)):
+  lst[i]=int(lst[i])+time
+for j in range(len(lst)):
+  if lst[j]<=5:
+    count+=1
+print(count//3)
