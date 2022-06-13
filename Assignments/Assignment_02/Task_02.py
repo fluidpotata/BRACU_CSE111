44b35e42050b08a8e385fe673941c8e6abd04405
Ibrahim Alif <100870354+itsalif1608@users.noreply.github.com>
2022-06-14 05:54:11 +0600

    Task 02

---
+++ Assignments/Assignment_02/Task_02.py
@@ -0,0 +1,28 @@
+class Flower:
+    name = ""
+    color = ""
+
+'''Fixed'''
+flower1 = Flower() 
+flower1.name="Rose" 
+flower1.color="Red" 
+flower1.num_of_petal=6 
+print("Name of this flower:", flower1.name) 
+print("Color of this flower:",flower1.color) 
+print("Number of petal:",flower1.num_of_petal) 
+print("=====================") 
+flower2 = Flower()
+flower2.name="Orchid" 
+flower2.color="Purple" 
+flower2.num_of_petal=4 
+print("Name of this flower:",flower2.name) 
+print("Color of this flower:",flower2.color) 
+print ("Number of petal:",flower2. num_of_petal)
+'''Can not change'''
+
+print(flower1)
+print(flower2)
+if flower1 == flower2:
+    print("They're the same")
+else:
+    print("They're different")
