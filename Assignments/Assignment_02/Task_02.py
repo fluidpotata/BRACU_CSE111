cebc6137d43f6b215c4a418c110756ab95b44bc6
Ibrahim Alif <100870354+itsalif1608@users.noreply.github.com>
2022-06-14 12:03:59 +0600

    Flower

--- Assignments/Assignment_02/Task_02.py
+++ Assignments/Assignment_02/Task_02.py
@@ -1,28 +1,33 @@
 class Flower:
-    name = ""
-    color = ""
+    def __init__(self,name = None,color = None,num = None):
+        self.name = name
+        self.color = color
+        self.num_of_petal = num
+        
+# Or can be created an empty class and  the code would still run
 
-'''Fixed'''
-flower1 = Flower() 
-flower1.name="Rose" 
-flower1.color="Red" 
-flower1.num_of_petal=6 
-print("Name of this flower:", flower1.name) 
-print("Color of this flower:",flower1.color) 
-print("Number of petal:",flower1.num_of_petal) 
-print("=====================") 
+
+
+flower1 = Flower()
+flower1.name="Rose"
+flower1.color="Red"
+flower1.num_of_petal=6
+print("Name of this flower:", flower1.name)
+print("Color of this flower:",flower1.color)
+print("Number of petal:",flower1.num_of_petal)
+print("=====================")
 flower2 = Flower()
-flower2.name="Orchid" 
-flower2.color="Purple" 
-flower2.num_of_petal=4 
-print("Name of this flower:",flower2.name) 
-print("Color of this flower:",flower2.color) 
+flower2.name="Orchid"
+flower2.color="Purple"
+flower2.num_of_petal=4
+print("Name of this flower:",flower2.name)
+print("Color of this flower:",flower2.color)
 print ("Number of petal:",flower2. num_of_petal)
-'''Can not change'''
+
 
 print(flower1)
 print(flower2)
 if flower1 == flower2:
-    print("They're the same")
+    print("They are the same")
 else:
-    print("They're different")
+    print("They are different")
