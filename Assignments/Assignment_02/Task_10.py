f358d4008e914171e07218a2fde3488087009eab
Ibrahim Alif <100870354+itsalif1608@users.noreply.github.com>
2022-06-14 06:05:38 +0600

    Wadiya

---
+++ Assignments/Assignment_02/Task_10.py
@@ -0,0 +1,18 @@
+class Wadiya(): 
+    def __init__(self): 
+        self.name = 'Aladeen' 
+        self.designation = 'President Prime Minister Admiral General' 
+        self.num_of_wife = 100 
+        self.dictator = True
+
+        
+wadiya = Wadiya()
+wadiya.part = 1
+print(f"Part {wadiya.part}: \nName of president: {wadiya.name} \nNumber of wife:{wadiya.num_of_wife} \nIs he/she dictator: {wadiya.dictator}")
+wadiya.part = 2
+wadiya.name = "Donald Trump"
+wadiya.designation = "President"
+wadiya.num_of_wife = 1
+wadiya.dictator = False
+print(f"Part {wadiya.part}: \nName of president: {wadiya.name} \nNumber of wife:{wadiya.num_of_wife} \nIs he/she dictator: {wadiya.dictator}")
+#subtask 4 is to do
