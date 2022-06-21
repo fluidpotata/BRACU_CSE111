cd63a0d1c2084a20c0662e72c21e8a519ef4912f
Ibrahim Alif <100870354+itsalif1608@users.noreply.github.com>
2022-06-21 12:09:18 +0600

    Patient

---
+++ Assignments/Assignment_03/Task_03.py
@@ -0,0 +1,23 @@
+# Write your code here
+class Patient:
+    def __init__(self,name,age,weight,height):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
+
+    def printDetails(self):
+        print(f"Name : {self.name}")
+        print(f"Age : {self.age}")
+        print(f"Weight : {self.weight}")
+        print(f"Height : {self.height}")
+        print(f"BMI : {self.weight / ((self.height/100)**2)}")
+
+        
+#===================================
+
+p1 = Patient("A", 55, 63.0, 158.0)
+p1.printDetails()
+print("====================")
+p2 = Patient("B", 53, 61.0, 149.0)
+p2.printDetails()
