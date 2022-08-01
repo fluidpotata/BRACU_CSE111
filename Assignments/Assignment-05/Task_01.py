89a9c35c994982534660eec2f2f6dba38c715855
Ibrahim Alif <100870354+itsalif1608@users.noreply.github.com>
2022-08-02 00:00:59 +0600

    Create Task_01.py

---
+++ Assignments/Assignment-05/Task_01.py
@@ -0,0 +1,20 @@
+#Write your code here
+class Marks:
+    def __init__(self,num):
+        self.mark = num
+
+
+    def __add__(self, other):
+        x = self.mark + other.mark
+        return Marks(x)
+
+#Do not change the following lines of code
+Q1 = Marks(int(input("Quiz 1 (out of 10): ")))
+Q2 = Marks(int(input("Quiz 2 (out of 10): ")))
+Lab = Marks(int(input("Lab (out of 30): ")))
+Mid = Marks(int(input("Mid (out of 20): ")))
+Final = Marks(int(input("Final (out of 30): ")))
+total = Q1 + Q2 + Lab + Mid + Final
+print("Total marks: {}".format(total.mark))
+
+
