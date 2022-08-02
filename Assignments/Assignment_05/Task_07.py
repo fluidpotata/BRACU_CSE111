e849cd85840d75472e6e36a8cd76b52387d1baa2
Ibrahim Alif <100870354+itsalif1608@users.noreply.github.com>
2022-08-02 11:49:33 +0600

    Create Task_07.py

---
+++ Assignments/Assignment_05/Task_07.py
@@ -0,0 +1,56 @@
+class Dolls:
+    def __init__(self,n,p):
+        self.name = n
+        self.price = p
+
+    def detail(self):
+        return f"Doll: {self.name} \nTotal Price: {self.price} taka"
+
+    def __gt__(self, other):
+        return self.price > other.price
+
+    def __add__(self, other):
+        x = self.name +" "+ other.name
+        y = self.price + other.price
+        return Dolls(x,y)
+
+# Write your code here
+
+obj_1 = Dolls("Tweety", 2500)
+print(obj_1.detail())
+if obj_1 > obj_1:
+    print("Congratulations! You get the Tweety as a gift!")
+else:
+    print("Thank you!")
+
+print("=========================")
+obj_2 = Dolls("Daffy Duck", 1800)
+print(obj_2.detail())
+if obj_2 > obj_1:
+    print("Congratulations! You get the Tweety as a gift!")
+else:
+    print("Thank you!")
+
+print("=========================")
+obj_3 = Dolls("Bugs Bunny", 3000)
+print(obj_3.detail())
+if obj_3 > obj_1:
+    print("Congratulations! You get the Tweety as a gift!")
+else:
+    print("Thank you!")
+
+print("=========================")
+obj_4 = Dolls("Porky Pig", 1500)
+print(obj_4.detail())
+if obj_4 > obj_1:
+    print("Congratulations! You get the Tweety as a gift!")
+else:
+    print("Thank you!")
+
+print("=========================")
+obj_5 = obj_2 + obj_3
+print(obj_5.detail())
+if obj_5 > obj_1:
+    print("Congratulations! You get the Tweety as a gift!")
+else:
+    print("Thank you!")
