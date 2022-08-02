1ffb006bd4718136f9c94b5144c3b735225f0e4b
Ibrahim Alif <100870354+itsalif1608@users.noreply.github.com>
2022-08-02 11:48:19 +0600

    Update Task_06.py

--- Assignments/Assignment_05/Task_06.py
+++ Assignments/Assignment_05/Task_06.py
@@ -3,6 +3,21 @@ class Triangle:
     def __init__(self,b,h):
         self.__base = b
         self.__height = h
+        self.__area =  .5*b*h
+
+    def getBase(self):
+        return  self.__base
+
+    def getHeight(self):
+        return self.__height
+
+    def area(self):
+        return self.__area
+
+    def __sub__(self, other):
+        x = self.__base - other.__base
+        y = self.__height - other.__height
+        return Triangle(x,y)
 
 
 t1 = Triangle(10, 5)
